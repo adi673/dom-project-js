555d8fa61bc61d257e9e1675366866d57791875e
Aditya Wadgaonkar <119002724+adi673@users.noreply.github.com>
2023-12-22 13:40:22 +0530

    Update chaiaurcode.js

---
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -0,0 +1,32 @@
+const form=document.querySelector('form')
+let Guess_remain=10
+const previous_guess=document.querySelector('.guesses')
+const Guess_left=document.querySelector('.lastResult')
+const min=1
+const max=100
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  const value=Math.floor(Math.random()*(max-min+1)+min)
+  console.log(value)
+  if(Guess_remain>0 ){
+    const input=parseInt(document.querySelector('input').value)
+    console.log(input)
+    Guess_remain=Guess_remain-1
+    if(value!=input){
+      previous_guess.innerHTML=input
+      Guess_left.innerHTML=Guess_remain
+
+      if(value>input){
+        document.querySelector('.lowOrHi').innerHTML="Guess little high"
+      }else if(value<input){
+        document.querySelector('.lowOrHi').innerHTML="Guess little low"
+      }
+    }else{
+      document.querySelector('.lowOrHi').innerHTML="You guessed Succesfully"
+    }
+  }else{
+    document.querySelector('.lowOrHi').innerHTML="Sorry You lost all the chances for guess"
+  }
+  
+})
